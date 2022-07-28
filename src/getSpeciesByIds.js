e275673775371547c1e9f01638739cf5ccfeade5
Richard P Machado <richardmachado2211@gmail.com>
2022-07-28 15:32:55 -0300

    1 passou em um teste falta o segundo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
-
+// 1- recerber varios parâmetros -- ...ids
+// 2.1 Se a função receber apenas um id, retorne a espécie do animal referente a este id; -- map e filter acho / reduce
+// 2.2 Se a função receber vários ids, retorne todas as espécies referente a esses ids. -- map e filter acho / ou reduce
+const getSpeciesByIds = (...ids) => ids.map((id) => species
+  .filter((especie) => especie.id === id));
 module.exports = getSpeciesByIds;
