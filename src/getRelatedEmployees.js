f0df547bfe959b5fdcd0413e737847664a083af5
Richard P Machado <richardmachado2211@gmail.com>
2022-07-28 20:44:52 -0300

    requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,7 +5,7 @@ const trabalhadores = [...data.employees];
 // Deve retornar true se o id passado for de uma pessoa gerente; -- some ou find
 // Deve retornar false se o id passado não for de uma pessoa gerente. -- some ou find
 
-// função getRelatedEmployees, caso o ID passado for da pessoa gerente, deve retornar um array contendo nome e sobrenome das pessoas colaboradoras pelas quais ela é responsável; w
+// função getRelatedEmployees, caso o ID passado for da pessoa gerente, deve retornar um array contendo nome e sobrenome das pessoas colaboradoras pelas quais ela é responsável; -- forEach e um push
 
 // A função getRelatedEmployees, caso o ID passado não for da pessoa gerente, deve disparar um erro com a mensagem: 'O id inserido não é de uma pessoa colaboradora gerente!'. -- Throw new Error
 
@@ -16,6 +16,13 @@ const getRelatedEmployees = (managerId) => {
   if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
+  const gerentes = [];
+  trabalhadores.forEach((colaborador) => {
+    if (colaborador.managers.includes(managerId)) {
+      gerentes.push(`${colaborador.firstName} ${colaborador.lastName}`);
+    }
+  });
+  return gerentes;
 };
 
 module.exports = { isManager, getRelatedEmployees };
