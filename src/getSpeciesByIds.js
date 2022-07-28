c3812db6bf3e1fb9b0d06006a9e24eb754f18d16
Richard P Machado <richardmachado2211@gmail.com>
2022-07-28 15:44:02 -0300

    Requisito 1 completo - trocando o filter por find

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 // 1- recerber varios parâmetros -- ...ids
-// 2.1 Se a função receber apenas um id, retorne a espécie do animal referente a este id; -- map e filter acho / reduce
-// 2.2 Se a função receber vários ids, retorne todas as espécies referente a esses ids. -- map e filter acho / ou reduce
+// 2.1 Se a função receber apenas um id, retorne a espécie do animal referente a este id; -- map e filter acho /ou reduce -- corrigindo map e find
+// 2.2 Se a função receber vários ids, retorne todas as espécies referente a esses ids. -- map e filter acho / ou reduce -- corrigindo map e find
 const getSpeciesByIds = (...ids) => ids.map((id) => species
-  .filter((especie) => especie.id === id));
+  .find((especie) => especie.id === id));
 module.exports = getSpeciesByIds;
