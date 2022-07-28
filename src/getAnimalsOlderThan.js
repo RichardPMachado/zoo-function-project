dacb55b2fef6221ff82c898768526056c9df45a6
Richard P Machado <richardmachado2211@gmail.com>
2022-07-28 17:19:28 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const especies = [...data.species]; // pegando array species
 
+// console.log(especies);
+// Implemente a função getAnimalsOlderThan que deve receber uma espécie e uma idade como parâmetro, e então retornar se todos os animais dessa espécie possuem essa idade ou são mais velhos.
+// e filter ou find para encontrar
+// se são TODOS e retorno boleano -- provavelmente every solucione
+
+const getAnimalsOlderThan = (animal, age) => especies.find((especie) => animal === especie.name)
+  .residents.every((especie) => especie.age >= age);
+// console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
