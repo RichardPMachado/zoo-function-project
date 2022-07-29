9c091dc1922c4f26d37ac6d4e4e54eff155d5d20
Richard P Machado <richardmachado2211@gmail.com>
2022-07-28 22:53:46 -0300

    requisito 5 - primeira parte resolvida com Object.FromEntries()

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const especies = [...data.species];
 
+const countAnimals = (animal) => {
+  if (!animal) {
+  // O método Object.fromEntries() transforma uma lista de pares chave-valor em um objeto. https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+    return Object.fromEntries(especies.map((especie) =>
+      [especie.name, especie.residents.length]));
+  }
+};
 module.exports = countAnimals;
