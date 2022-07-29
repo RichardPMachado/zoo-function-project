1f5f556fa72a2f3ffb267e585820d66475a2709e
Richard P Machado <richardmachado2211@gmail.com>
2022-07-29 00:23:53 -0300

    requisito 5.2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,11 +2,15 @@ const data = require('../data/zoo_data');
 
 const especies = [...data.species];
 
+// O método Object.fromEntries() transforma uma lista de pares chave-valor em um objeto. https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+const contarTodosAnimais = Object.fromEntries(especies.map((especie) =>
+  [especie.name, especie.residents.length]));
 const countAnimals = (animal) => {
   if (!animal) {
-  // O método Object.fromEntries() transforma uma lista de pares chave-valor em um objeto. https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
-    return Object.fromEntries(especies.map((especie) =>
-      [especie.name, especie.residents.length]));
+    return contarTodosAnimais;
   }
+  return especies.find((especie) => especie.name === animal.specie).residents.length;
+  // especies.map((especie) => especie.include(animal)
+  //   especie.name, especie.residents.length);
 };
 module.exports = countAnimals;
