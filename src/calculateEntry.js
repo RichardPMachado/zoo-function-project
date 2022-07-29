11edad4517f4f94c684f2478d38f18f0f38c1617
Richard P Machado <richardmachado2211@gmail.com>
2022-07-29 15:58:11 -0300

    requisito 8 - completo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+// console.log(prices.child);
+const countEntrants = (entrants) => entrants.reduce((acc, curr) => {
+  if (curr.age < 18) acc.child += 1;
+  else if (curr.age < 50) acc.adult += 1;
+  else acc.senior += 1;
+  return acc;
+}, { adult: 0, child: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const a = countEntrants(entrants);
+  const total = a.adult * prices.adult
+  + a.child * prices.child
+  + a.senior * prices.senior;
+  return total;
 };
-// inicio
+
 module.exports = { calculateEntry, countEntrants };
