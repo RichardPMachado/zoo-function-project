1670166f6e433571bf85a14542d590773f87a260
Richard P Machado <richardmachado2211@gmail.com>
2022-07-29 03:15:59 -0300

    requisito 6 - completo

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,43 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Test 1', () => {
+    const actual = handlerElephants('count');
+    expect(actual).toBe(4);
+  });
+  it('Test 2', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+  it('Test 3', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+  it('Test 4', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+  it('Test 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+  it('Test 6', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+  it('Test 7', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toEqual(expected);
+  });
+  it('Test 8', () => {
+    const actual = handlerElephants('id');
+    const expected = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
+    expect(actual).toEqual(expected);
+  });
+});
