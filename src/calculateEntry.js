14a0960a6928c469cb041ced39a4cd8943d60bc1
Richard P Machado <richardmachado2211@gmail.com>
2022-07-29 03:23:50 -0300

    parei no requisito 8 - retorno amanha

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const countEntrants = (entrants) => {
   // seu código aqui
-}
+};
 
-function calculateEntry(entrants) {
+const calculateEntry = (entrants) => {
   // seu código aqui
-}
+};
 // inicio
 module.exports = { calculateEntry, countEntrants };
