e971091b7c47252a91b48f8ce917b0f12d179aec
Richard P Machado <richardmachado2211@gmail.com>
2022-07-31 17:23:40 -0300

    requisito 10 - completo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species } = data;
 
+const { employees } = data;
+
+const procuraEspecieMaiorIdade = (id) => {
+  const especie = species.filter((e) => e.id === id)[0].residents;
+  const especieIdades = especie.map((e) => e.age);
+  const especieMaiorIdade = especieIdades.sort((a, b) => a - b);
+  const animalDados = especie
+    .find((e) => e.age === especieMaiorIdade[especieMaiorIdade.length - 1]);
+  return [animalDados.name, animalDados.sex, animalDados.age];
+};
+const getOldestFromFirstSpecies = (id) => {
+  const trabalhador = employees.filter((e) => e.id === id)
+    .map((animais) => animais.responsibleFor)[0][0];
+  const a = procuraEspecieMaiorIdade(trabalhador);
+
+  return a;
+};
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
