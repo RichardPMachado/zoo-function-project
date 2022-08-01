980862ac83747d68c1955933c412d4e7ae1fbfba
Richard P Machado <richardmachado2211@gmail.com>
2022-07-31 21:54:01 -0300

    requisito 11 - completo (refatoramento)

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data;
+
+const { employees } = data;
+
+const funcionario = (elemento) => employees.find((e) => e.firstName === elemento.name
+    || elemento.id === e.id
+    || elemento.name === e.lastName);
+
+console.log(funcionario({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
+
+const buscarAnimais = (id) => species.filter((e) => e.id === id)[0].name;
+
+const buscarLocais = (id) => species.filter((e) => e.id === id)[0].location;
+
+const infosTrabalhador = (elemento) => {
+  const dadosTrabalhador = {};
+  const dadosFuncionario = funcionario(elemento);
+  dadosTrabalhador.id = dadosFuncionario.id;
+  dadosTrabalhador.fullName = `${dadosFuncionario.firstName} ${dadosFuncionario.lastName}`;
+  dadosTrabalhador.species = dadosFuncionario.responsibleFor
+    .map((e) => buscarAnimais(e));
+  dadosTrabalhador.locations = dadosFuncionario.responsibleFor
+    .map((e) => buscarLocais(e));
+  return dadosTrabalhador;
+};
+
+function getEmployeesCoverage(elemento) {
+  const a = [];
+  if (!elemento) {
+    employees.forEach((acc) => a.push(infosTrabalhador(acc)));
+    return a;
+  }
+  const trabalhador = funcionario(elemento);
+  if (!trabalhador) throw new Error('Informações inválidas');
+  return infosTrabalhador(trabalhador);
 }
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
