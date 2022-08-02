d53c292835199503f1f6874bd82c6e37770593ec
Richard P Machado <richardmachado2211@gmail.com>
2022-08-02 13:29:04 -0300

    requisito 9 - 3/5 passed

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,27 +1,17 @@
 const data = require('../data/zoo_data');
 
 const especies = [...data.species];
-// const horas = { ...data.hours };
-const a = especies.filter((especie) => especie.availability === 'Tuesday');
-console.log(a);
-// const officeHour = (aberto, fechado) => {
-//   if (aberto === 0) {
+
+const horas = { ...data.hours };
+console.log(horas);
+// const diasSemana = (dia) => dia.map((e) => {
+//   if (horas[e].open === 0) {
 //     return 'CLOSED';
 //   }
-//   return `Open from ${aberto}am until ${fechado}pm`;
-// };
+//   return `Open from ${horas[e].open}am until ${horas[e].close}pm`;
+// });
 
-// const animaisDia = (dia) => {
-//   const exhibition = [];
-//   especies.find((especie) => especie.availability === dia);
-// };
-
-// dia = {
-//   diaDaSemana {
-//     officeHour: `Open from ${horaAm}am until ${horaPm}pm`,
-//     exhibition: exibition,
-//   }
-// }
+// console.log(diasDaSemana);
 
 // reduce((acc, curr) => cosole.log(Object.values(acc)), {});
 /*
@@ -30,47 +20,61 @@ const expected = {
   'Tuesday': {
     'officeHour': 'Open from 8am until 6pm',
     'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
-  },
-  'Wednesday': {
-    'officeHour': 'Open from 8am until 6pm',
-    'exhibition': [ 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes' ],
-  },
-  'Thursday': {
-    'officeHour': 'Open from 10am until 8pm',
-    'exhibition': [ 'lions', 'otters', 'frogs', 'snakes', 'giraffes' ],
-  },
-  'Friday': {
-    'officeHour': 'Open from 10am until 8pm',
-    'exhibition': [ 'tigers', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes' ],
-  },
-  'Saturday': {
-    'officeHour': 'Open from 8am until 10pm',
-    'exhibition': [
-      'lions',  'tigers',
-      'bears',  'penguins',
-      'otters', 'frogs',
-      'snakes', 'elephants',
-    ],
-  },
-  'Sunday': {
-    'officeHour': 'Open from 8am until 8pm',
-    'exhibition': [ 'lions', 'bears', 'penguins', 'snakes', 'elephants' ],
+  }
   },
   'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
 };
-*/
-// const animais = (animal) => {
-//   const bicho = especies.find((especie) => especie.name === animal);
-//   return bicho.availability;
-// };
+          ************TESTES E COISAS PARA USAR************
+==================================================================================
+{"Friday": {"exhibition": ["tigers", "otters", "frogs", "snakes", "elephants", "giraffes"], "officeHour": "Open from 10am until 8pm"}, "Monday": {"exhibition": "The zoo will be closed!", "officeHour": "CLOSED"}, "Saturday": {"exhibition": ["lions", "tigers", "bears", "penguins", "otters", "frogs", "snakes", "elephants"], "officeHour": "Open from 8am until 10pm"}, "Sunday": {"exhibition": ["lions", "bears", "penguins", "snakes", "elephants"], "officeHour": "Open from 8am until 8pm"}, "Thursday": {"exhibition": ["lions", "otters", "frogs", "snakes", "giraffes"], "officeHour": "Open from 10am until 8pm"}, "Tuesday": {"exhibition": ["lions", "tigers", "bears", "penguins", "elephants", "giraffes"], "officeHour": "Open from 8am until 6pm"}, "Wednesday": {"exhibition": ["tigers", "bears", "penguins", "otters", "frogs", "giraffes"], "officeHour": "Open from 8am until 6pm"}}
+const horarioDeFuncionamento = (diaDaSemana) => {
+  const objVazio = { [diaDaSemana]: {
+    officeHour: horas[diaDaSemana].open === 0 ? 'CLOSED'
+      : `Open from ${horas[diaDaSemana].open}am until ${horas[diaDaSemana].close}pm`,
+    exhibition: horas[diaDaSemana].open === 0
+      ? 'The zoo will be closed!'
+      : especies
+        .filter((elemento) => elemento.availability.includes(diaDaSemana))
+        .map((elemento) => elemento.name),
+  },
+  };
+  return objVazio;
+};
 
-// const objVazio = () => {}
+*/
+const buscarAnimais = (elemento) => {
+  const animal = especies.filter((especie) => especie.name === elemento);
+  return animal.map((e) => e.availability)[0];
+};
+const diasDaSemana = Object.keys(horas);
+const horarioDeFuncionamento = (diaDaSemana) => {
+  const objVazio = { [diaDaSemana]: {
+    officeHour: horas[diaDaSemana].open === 0 ? 'CLOSED'
+      : `Open from ${horas[diaDaSemana].open}am until ${horas[diaDaSemana].close}pm`,
+    exhibition: horas[diaDaSemana].open === 0
+      ? 'The zoo will be closed!'
+      : especies
+        .filter((elemento) => elemento.availability.includes(diaDaSemana))
+        .map((elemento) => elemento.name),
+  },
+  };
+  return objVazio;
+};
+
+// console.log(horarioDeFuncionamento('Tuesday'));
+const animaisNome = especies.map(({ name }) => name);
+const diasAnimais = animaisNome.concat(diasDaSemana);
 
 const getSchedule = (scheduleTarget) => {
-  // return animais(scheduleTarget);
-  // officeHour: `'Open from ${horas.open}am until ${horas.close}pm'`,
-  // exhibition:
+  if (diasDaSemana.includes(scheduleTarget)) {
+    return horarioDeFuncionamento(scheduleTarget);
+  }
+  if (!diasAnimais.includes(scheduleTarget) || Object.entries(scheduleTarget).length === 0) {
+    const a = diasDaSemana.map((e) => horarioDeFuncionamento(e));
+    return a;
+  }
+  return buscarAnimais(scheduleTarget);
 };
-console.log(getSchedule('lions'));
+// console.log(getSchedule());
 
 module.exports = getSchedule;
