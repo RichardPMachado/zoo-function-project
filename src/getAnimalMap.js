868d6b28c4b2790566daf3d7ce7ec86f023ad9b0
Richard P Machado <richardmachado2211@gmail.com>
2022-08-06 18:53:30 -0300

    requisito 14 - completo, falta refatorar

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,60 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+const locais = species.map((e) => e.location);
+
+const todosAnimaisLocais = () => {
+  const objVazio = locais.reduce((acc, curr) => {
+    acc[curr] = species.filter((elemento) => elemento.location.includes(curr))
+      .map((elemento) => elemento.name);
+    return acc;
+  }, {});
+  return objVazio;
+};
+
+const nomeTodosAnimais = (especies, sex, sorted) => {
+  const objVazio = {};
+  const residentes = species.find((especie) => especie.name === especies).residents;
+  if (sex === undefined) {
+    // console.log(sex);
+    if (sorted === false) {
+      objVazio[especies] = residentes.map((e) => e.name);
+    } else objVazio[especies] = residentes.map((e) => e.name).sort();
+  } else {
+    if (sorted === false) {
+      objVazio[especies] = residentes.filter((e) => e.sex === sex)
+        .map((e) => e.name);
+    } else {
+        objVazio[especies] = residentes.filter((e) => e.sex === sex)
+        .map((e) => e.name).sort();
+    }
+  }
+  // console.log(objVazio);
+  return objVazio;
+};
+
+// console.log(nomeTodosAnimais('lions'));
+const verificaIncludes = (sex = undefined, sorted = false) => {
+  console.log(sex, sorted);
+  const objVazio = locais.reduce((acc, curr) => {
+    acc[curr] = species.filter((elemento) => elemento.location.includes(curr))
+      .map((elemento) => nomeTodosAnimais(elemento.name, sex, sorted));
+    // console.log(acc[curr]);
+    return acc;
+  }, {});
+  return objVazio;
+};
+// console.log(verificaIncludes({ sorted: false }).NE);
+const verificaOpcoes = (options) => {
+  if (options.sex === 'female' || options.sorted === true) {
+    return verificaIncludes(options.sex, options.sorted);
+  } return verificaIncludes(undefined, false);
+};
+// console.log(animaisLocais());
+function getAnimalMap(options = {}) {
+  return options.includeNames ? verificaOpcoes(options) : todosAnimaisLocais();
 }
+// console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
